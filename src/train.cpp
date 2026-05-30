1e02a0176cff9d97404dd904823f09e4059b8bb7
VarvaraSed <varvarasedyshevafgh@gmail.com>
2026-05-30 17:12:04 +0300

    Седышева В.В_25ИСТ4-1_ads7

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,62 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2026 NNTU-CS
 #include "train.h"
+
+Train::Train() : countOp(0), first(nullptr) {}
+
+void Train::addCar(bool light) {
+    Car* car = new Car{ light, nullptr, nullptr };
+
+    if (first == nullptr) {
+        first = car;
+        first->next = first;
+        first->prev = first;
+    } else {
+        Car* last = first->prev;
+        last->next = car;
+        car->prev = last;
+        car->next = first;
+        first->prev = car;
+    }
+}
+
+int Train::getLength() {
+    if (first == nullptr) return 0;
+    countOp = 0;
+    first->light = false;
+    Car* current = first->next;
+    countOp++;
+    while (true) {
+        if (current->light) {
+            current->light = false;
+            for (Car* back = current->prev; back != first; back = back->prev) {
+                countOp++;
+            }
+            countOp++;
+
+            current = first->next;
+            countOp++;
+        } else {
+            current = current->next;
+            countOp++;
+        }
+
+        if (current == first && !current->light) {
+            break;
+        }
+    }
+
+    int length = 0;
+    Car* temp = first;
+    do {
+        length++;
+        temp = temp->next;
+        countOp++;
+    } while (temp != first);
+    countOp--;
+
+    return length;
+}
+
+int Train::getOpCount() const {
+    return countOp;
+}
